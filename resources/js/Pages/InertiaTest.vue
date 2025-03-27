6a7345d013c3816e9314b2f119ba6e7f54f6a5a6
shogo109 <rodorigesthethird0189@gmail.com>
2025-03-28 00:33:12 +0900

    add: itemの下準備

---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { Link } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <div>
+        <h1>Inertia Test</h1>
+    </div>
+    <a href="/">aタグ経由</a>
+    <Link href="/">Linkコンポーネント経由</Link>
+    <Link :href="route('inertia.index')">Inertia経由</Link>
+</template>
