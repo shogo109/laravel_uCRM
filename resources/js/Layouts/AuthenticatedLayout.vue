c49a7c51adb1e989e9028a81531794d47bae8b0f
shogo109 <rodorigesthethird0189@gmail.com>
2025-06-11 15:41:25 +0900

    日別グラフ

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -61,6 +61,12 @@ const showingNavigationDropdown = ref(false);
                                 >
                                     顧客管理
                                 </NavLink>
+                                <NavLink
+                                    :href="route('analysis')"
+                                    :active="route().current('analysis')"
+                                >
+                                    データ分析
+                                </NavLink>
                             </div>
                         </div>
 
@@ -191,6 +197,12 @@ const showingNavigationDropdown = ref(false);
                             :active="route().current('customers.index')"
                         >
                             顧客管理
+                        </ResponsiveNavLink>
+                        <ResponsiveNavLink
+                            :href="route('analysis')"
+                            :active="route().current('analysis')"
+                        >
+                            データ分析
                         </ResponsiveNavLink>
                     </div>
 
